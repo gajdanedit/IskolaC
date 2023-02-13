f4a47b2e4df949f8a43b629cbc3218814f9bf466
gajdanedit <113353453+gajdanedit@users.noreply.github.com>
2023-02-13 14:11:50 +0100

    Add files via upload

---
+++ TestApp13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy összeget: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int c = 0;
+            int[] b = new int[] { 10, 20, 50, 100, 200 };
+            for (int i = b.Length; i > 0; i--)
+            {
+                while (a > b[i]) 
+                {
+                    a -= b[i];
+                    c++;
+                }
+            }
+            Console.WriteLine("Az összeg a lehető legkevesebb pénzérméből összeállítva: " + c + " db.");
+            Console.ReadLine();
+        }
+    }
+}
