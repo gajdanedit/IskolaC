5a68592b9eb911c2f8dad3e700fce449045dff9d
gajdanedit <113353453+gajdanedit@users.noreply.github.com>
2023-02-23 11:10:56 +0100

    Add files via upload

---
+++ Négyszög/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negyszog
+{
+    internal class Program
+    {
+        class negyszog
+        {
+            //osztályváltozók
+            private int a;
+            private int b;
+            private int terulet;
+            private int kerulet;
+
+            //konstruktorok
+            public negyszog() { }
+
+            public negyszog(int a, int b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+
+            public negyszog(int a)
+            { 
+              this.a = a;
+              this.b = a;
+            }
+            //metódusok
+            public void setA(int a) 
+            {
+                if (a > 1000)
+                {
+                    Console.WriteLine("Az oldal nem lehet nagyobb, mint 1000!");
+                }else
+                this.a = a; 
+            }
+            public void setB(int b) { this.b = b; }
+
+            public int getA() { return this.a; }
+            public int getB() { return this.b; }
+
+            public void setTerulet() 
+            {
+                this.terulet = this.a * this.b;
+            }
+            public int getTerulet() 
+            {
+                return this.terulet;
+            }
+            public void setKerulet()
+            {
+                this.kerulet = 2 * (this.a + this.b);
+            }
+            public int getKerulet()
+            {
+                return this.kerulet;
+            }
+        }
+        enum nyomtat { oldal, kerter}
+        static void Main(string[] args)
+        {
+            //első példa
+            negyszog n1= new negyszog();
+            kiir(n1, nyomtat.oldal);
+
+            n1.setA(5);
+            n1.setB(6);
+
+            kiir(n1, nyomtat.oldal);
+
+            n1.setTerulet();
+            n1.setKerulet();
+            kiir(n1, nyomtat.kerter);
+
+
+            //2. példa
+            negyszog n2= new negyszog(6, 2);
+
+            kiir(n2, nyomtat.oldal);
+            n2.setTerulet();
+            n2.setKerulet();
+            kiir(n2, nyomtat.kerter);
+
+            //3. negyzet pelda
+            negyszog n3 = new negyszog(6);
+
+            kiir(n3, nyomtat.oldal);
+            n3.setTerulet();
+            n3.setKerulet();
+            kiir(n3, nyomtat.kerter);
+
+            Console.ReadKey();
+        }
+        static private void kiir(negyszog obj, nyomtat v)
+        {
+            if (v == nyomtat.oldal)
+            {
+                Console.WriteLine($"Az négyszög oldalai: a = {obj.getA()}, b = {obj.getB()}");
+            }
+            else
+                Console.WriteLine($"Az négyszög területe = {obj.getTerulet()}, kerülete = {obj.getKerulet()}");
+        }
+    }
+}
---
+++ kor/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kor
+{
+     class Kor
+    {
+        private int r;
+        private double terulet;
+        private double kerulet;
+
+        public Kor() { }
+
+        public Kor(int r){
+        this.r = r;
+        }
+        public void setR(int r)
+        {
+            this.r = r;
+        }
+
+        public int getR() 
+        {
+            return this.r; 
+        }
+        public void setTerulet()
+        {
+            this.terulet = this.r*this.r*Math.PI;
+        }
+        public double getTerulet()
+        {
+            return this.terulet;
+        }
+        public double getKerulet()
+        {
+            return this.kerulet;
+        }
+        public void setKerulet()
+        {
+            this.kerulet = 2*this.r * Math.PI;
+        }
+
+
+
+    }
+        internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            Kor kor = new Kor();
+            kor.setR(1);
+            kor.setTerulet();
+            kor.setKerulet();
+            Console.WriteLine($"A kör területe: {kor.getTerulet()}, kerülete:, {kor.getKerulet()}");
+
+            Console.ReadKey();
+        }
+    }
+}
