89f082218755aaeef9ef38cc97f126ecaf216dc5
gajdanedit <113353453+gajdanedit@users.noreply.github.com>
2023-03-10 10:37:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atlaghomerseklet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matrix = new int[5, 10];
+            for (int i =0; i <5; i++) 
+            {
+                for (int j = 0 ; j < 10; j++) 
+                {
+                    Console.WriteLine("{0}\t", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            #region Függvényekkel való használata
+            Console.WriteLine("Ez a függvénnyel létrehozott mátrix: ");
+            int[,] ezisegymatrix
+        }
+    }
+}
